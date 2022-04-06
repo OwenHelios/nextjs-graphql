1c727f9c72323f50bc550c2a8b898fe304697b9e
OwenHelios <owenhelios@outlook.com>
2022-04-06 10:19:53 +0800

    graphql fragment

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,37 @@
-import { Box, Flex, Link } from '@chakra-ui/react'
+import { Box, Button, Flex, Link } from '@chakra-ui/react'
 import NextLink from 'next/link'
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery()
+  let body = null
+
+  if (fetching) {
+    //data is loading
+  } else if (!data?.me) {
+    //not logged in
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link color="white" mr={4}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white">Register</Link>
+        </NextLink>
+      </>
+    )
+  } else {
+    //logged in
+    body = (
+      <Flex>
+        <Box mr={4}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    )
+  }
   return (
     <Box bg="tomato" p={4}>
       <Flex ml={'auto'} width="fit-content">
